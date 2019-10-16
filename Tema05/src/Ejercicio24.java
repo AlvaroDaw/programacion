4e51da71678385029756cb0898f7412d6f82bb89
alvaro.daw@gmail.com <alvaro.daw@gmail.com>
2019-10-16 10:11:26 +0200

    Actualizando

---
+++ Tema05/src/Ejercicio24.java
@@ -0,0 +1,14 @@
+/**
+ * Escribe un programa que lea un numero n e imprima una piramide de numeros con n filas
+ * como en la figura.
+ * 
+ * @author Alvaro Ramirez
+ *
+ */
+import java.util.Scanner;
+public class Ejercicio24 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+	}
+
+}
