ef998a7b49551845656fce5972c8bd83ecd5368e
alvaro.daw@gmail.com <alvaro.daw@gmail.com>
2019-10-16 08:32:31 +0200

    Actualizando

---
+++ Tema05/src/Ejercicio21.java
@@ -0,0 +1,11 @@
+/**
+ * Ejercicio 21
+ * Realiza un programa que vaya pidiendo numeros hasta que se introduzca un numero negativo y
+ * nos diga cuantos numeros se han introducido, la media de los impares y el mayor de los pares. 
+ * El numero negativo solo se utiliza para indicar el final de la introduccion
+ * @author Usuario
+ *
+ */
+public class Ejercicio21 {
+
+}
